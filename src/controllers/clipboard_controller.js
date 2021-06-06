e85a2a7f5f8677120f78effcde40d83560c6ab39
Nadia Salmen <nadiasalmen@gmail.com>
2021-06-06 17:38:38 -0300

    Progressively Enhancing the PIN Field

--- src/controllers/clipboard_controller.js
+++ src/controllers/clipboard_controller.js
@@ -2,7 +2,13 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   static targets = [ "source" ]
-  copy() {
+  connect() {
+    if (document.queryCommandSupported("copy")) {
+      this.element.classList.add("clipboard--supported")
+    }
+  }
+
+  copy(event) {
     event.preventDefault()
     this.sourceTarget.select()
     document.execCommand("copy")
